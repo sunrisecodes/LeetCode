f4f46cf51cdee78b12c2f28a7150331e0a0bd7c2
Shrouk Matar <45500394+ShroukMatter@users.noreply.github.com>
2024-08-02 06:33:42 +0300

    [1512] [Time Beats: 49.33%] [Memory Beats: 64.40%] - LeetPush

--- 1512-Number-of-Good-Pairs.cs
+++ 1512-Number-of-Good-Pairs.cs
@@ -1,20 +1,25 @@
 public class Solution {
   public int NumIdenticalPairs(int[] nums)
- { 
-     Dictionary<int,int> NumPairs = new Dictionary<int,int>();
-     int Pairscounter = 0;
-     foreach (int item in nums)
-     {
-
-           //New occurs of Item ?
+{ 
+    Dictionary<int,int> NumPairs = new Dictionary<int,int>();
+    int Pairscounter = 0;
+    foreach (int item in nums)
+    {
+        if (NumPairs.ContainsKey(item))
+        {
+            // New occurs of Item ?
             //How many times the Item appears before ?
-            //Num of previous occurs  * new occurs ==> Num of Pairs can this item make         
-          int count = NumPairs.GetValueOrDefault(item, 0);
-          Pairscounter += count;
-          NumPairs[item] = count+1;
-         
-     }
+            //Num of previous occurs  * new occurs ==> Num of Pairs can this item make
+            Pairscounter+= NumPairs[item];
+            NumPairs[item]++;
+        }
+        else
+        {
+            NumPairs[item] = 1;
+        }
+        
+    }
 
-     return Pairscounter;
- }
+    return Pairscounter;
+}
 }
