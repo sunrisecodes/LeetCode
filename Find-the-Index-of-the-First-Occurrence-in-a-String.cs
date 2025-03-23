fcbc74f2a4fe6389530e1aad5e094d97d95f91bd
Shrouk Ahmed <45500394+sunrisecodes@users.noreply.github.com>
2025-03-23 21:17:57 +0200

    [28] [Time Beats: 100.00%] [Memory Beats: 74.42%] - LeetPush

--- Find-the-Index-of-the-First-Occurrence-in-a-String.cs
+++ Find-the-Index-of-the-First-Occurrence-in-a-String.cs
@@ -1,37 +1,26 @@
 public class Solution {
-           public int StrStr(string haystack, string needle)
-        {
-            int found = -1;
-            int i = 0, j = 0;
-            while (i < haystack.Length)
-            {
-                if (haystack[i] == needle[j])
-                {
-                    int start = i;
-                    i++;
-                    j++;
-                    while (i < haystack.Length && j < needle.Length)
-                    {
-                        if (haystack[i] != needle[j])
-                            break;
+          public int StrStr(string haystack, string needle)
+ {
+     int i = 0, j = 0;
+     while (i < haystack.Length)
+     {
+         if (haystack[i] == needle[j])
+         {
+             int start = i;
+             while (i < haystack.Length && j < needle.Length && haystack[i] == needle[j] )
+             {
+                 i++;
+                 j++;
+             }
+             if (j == needle.Length) return start;
+             else
+                 j = 0;
+                 i = start;
+         }
+         i++;
+     }
+     return -1;
 
-                        i++;
-                        j++;
-                    }
-                    if (j == needle.Length)
-                    {
-                        found = start;
-                        return found;
-                    }
-                    else
-                        j = 0;
-                        i = start;
-                }
-                i++;
-            }
-            return found;
-
-        }
-
+ }
 
 }
